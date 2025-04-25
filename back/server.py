a993e5a06ae2ec573eb3aecccce6e244a996bf29
Rahul Kasimikota <125949760+rahul-9429@users.noreply.github.com>
2025-04-26 05:29:26 +0530

    Update server.py to match Cloud deployment

--- back/server.py
+++ back/server.py
@@ -111,4 +111,4 @@ def predict():
         return jsonify({"error": f"Failed to process request: {str(e)}"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
